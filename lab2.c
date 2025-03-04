7c0b9a4c01702caa82938f998d59a2ed0a146e7e
adwyck-columbia <ag5016@columbia.edu>
2025-03-03 20:45:24 -0500

    update_clear

--- lab2.c
+++ lab2.c
@@ -62,7 +62,7 @@ int main()
 for (rows = 0 ; rows < 24 ; col++){
 for (col = 0 ; col < 64 ; col++) {
   
-  fbputchar('', rows, col);
+  fbputchar(' ', rows, col);
   //fbputchar('*', 23, col);
 }
 }
