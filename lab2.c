654447414ee408d90936d49b11cee37c8c708dcd
adwyck-columbia <ag5016@columbia.edu>
2025-03-04 17:40:32 -0500

    ASCII

--- lab2.c
+++ lab2.c
@@ -123,11 +123,19 @@ fbputs("This is a long text that will automatically wrap.", 21, 50);
       sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],
 	      packet.keycode[1]);
       
-      if(packet.keycode[0] == 0 && packet.keycode[1] == 0 && packet.modifiers == 0){
+      // if(packet.keycode[0] == 0 && packet.keycode[1] == 0 && packet.modifiers == 0){
+      //   continue;
+      // }
+      // else{
+      
+      //   key = usbkey_to_ascii(packet.keycode[0], packet.modifiers);
+      // }
+
+      if(packet.keycode[0] == 0 && packet.modifiers == 0){
         continue;
       }
       else{
-      key = usbkey_to_ascii(packet.keycode[0], packet.modifiers);
+        key = usbkey_to_ascii(packet.keycode[0], packet.modifiers);
       }
 
     //  printf("%s\n", keystate);
