acbcfc195d489f05e3bc483ff99e867fdc4f9b78
adwyck-columbia <ag5016@columbia.edu>
2025-03-06 17:06:36 -0500

    Fiinal touches

--- lab2.c
+++ lab2.c
@@ -136,8 +136,7 @@ fbclear(0,0,0);
 			      (unsigned char *) &packet, sizeof(packet),
 			      &transferred, 0);
     if (transferred == sizeof(packet)) {
-     // sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],   
-	   //   packet.keycode[1]);
+      sprintf(keystate, "%02x %02x %02x", packet.modifiers, packet.keycode[0],packet.keycode[1]);
 
       if(packet.keycode[0] == 0 && packet.keycode[1] == 0 && packet.modifiers == 0){
         continue;
@@ -284,7 +283,7 @@ fbclear(0,0,0);
       ////////////////////////////////////////////
 
 
-      printf("%s\n", keystate);
+     // printf("%s\n", keystate);
     //  printf("%c\n", key);  //Current
 
     //printf("Key: %c, Buffer: \"%s\"\n", key, input_buffer);
